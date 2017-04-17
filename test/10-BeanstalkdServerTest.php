ef80eefd67d4372fc120f5bf7da54048cb3e7a6b
mle <maximilian@eul.cc>
2017-04-18 00:27:39 +0200

    ensure there's no other beanstalkd reachable in container

--- test/10-BeanstalkdServerTest.php
+++ test/10-BeanstalkdServerTest.php
@@ -18,6 +18,20 @@ class BeanstalkdServerTest
 			"No BEANSTALKD_PORT ENV variable found! Is this test running in the test container?");
 		$this->assertGreaterThan(1024, $_ENV['BEANSTALKD_PORT'],
 			"Invalid BEANSTALKD_PORT ENV variable!");
+		$this->assertNotEquals(PheanstalkInterface::DEFAULT_PORT , $_ENV['BEANSTALKD_PORT'],
+			"BEANSTALKD_PORT ENV variable should NOT be set to the default Beanstalkd port! " .
+			"This prevents the test scripts from accidentally running on the host system.");
+
+		$e = null;
+		try {
+			(new BeanstalkdWorkServer ("localhost", PheanstalkInterface::DEFAULT_PORT))
+				->getNextQueueEntry("@this-should-not-exist-29743984375345", BeanstalkdWorkServer::NOBLOCK);
+		} catch (\Pheanstalk\Exception\ConnectionException $e) {
+			// ok
+		}
+		$this->assertInstanceOf(\Exception::class, $e,
+			"We managed to get a Beanstalkd connection on the Beanstalk default port! " .
+			"This should not be possible inside the test container.");
 	}
 
 	public function getWorkServerAdapter () : WorkServerAdapter {
