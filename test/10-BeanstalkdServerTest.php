fc2d635f1a3b180666230f04b0b9879cbde10c09
mle <maximilian@eul.cc>
2017-04-18 00:20:30 +0200

    added test class

---
+++ test/10-BeanstalkdServerTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace mle86\WQ\Tests;
+
+use mle86\WQ\WorkServerAdapter\WorkServerAdapter;
+use mle86\WQ\WorkServerAdapter\BeanstalkdWorkServer;
+use Pheanstalk\PheanstalkInterface;
+
+require_once __DIR__.'/../vendor/mle86/wq/test/helper/AbstractWorkServerAdapterTest.php';
+
+class BeanstalkdServerTest
+	extends AbstractWorkServerAdapterTest
+{
+
+	public function getWorkServerAdapter () : WorkServerAdapter {
+		return new BeanstalkdWorkServer ("localhost");
+	}
+
+}
+
