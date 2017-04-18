7969a11a245c50defd9e569c2a55e17474ea2d7f
mle <maximilian@eul.cc>
2017-04-18 13:24:09 +0200

    more docker paranoia checks

--- docker-start.sh
+++ docker-start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ ! -f /.dockerenv ]; then
+	echo "This script is supposed to be run inside a Docker container."  >&2
+	echo "Try 'make test' instead."  >&2
+	exit 1
+fi
+
 beanstalkd  -u daemon  -p $BEANSTALKD_PORT  &
 
 exec "$@"
