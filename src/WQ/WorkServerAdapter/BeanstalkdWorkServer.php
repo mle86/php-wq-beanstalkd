576e82696cd87c88013bc2f33ca371edc55a0e13
mle <maximilian@eul.cc>
2017-09-23 10:32:05 +0200

    cs, PSR-2

--- src/WQ/WorkServerAdapter/BeanstalkdWorkServer.php
+++ src/WQ/WorkServerAdapter/BeanstalkdWorkServer.php
@@ -22,173 +22,184 @@ use Pheanstalk\PheanstalkInterface;
  * @see https://github.com/pda/pheanstalk  Uses the pda/pheanstalk package by Paul Annesley
  */
 class BeanstalkdWorkServer
-	implements WorkServerAdapter
+    implements WorkServerAdapter
 {
 
-	/** @var Pheanstalk */
-	private $ph;
-	/** @var array|null */
-	private $lastWatched = ["default"];  // By default, all clients are watching the "default" tube. We don't want that.
+    /** @var Pheanstalk */
+    private $ph;
+    /** @var array|null */
+    private $lastWatched = ["default"];  // By default, all clients are watching the "default" tube. We don't want that.
 
 
-	/**
-	 * Constructor.
-	 * Takes an already-configured {@see Pheanstalk} instance to work with.
-	 * Does not attempt to establish a connection itself --
-	 * use the {@see connect()} factory method for that instead.
-	 *
-	 * @param Pheanstalk $ph
-	 */
-	public function __construct (Pheanstalk $ph) {
-		$this->ph = $ph;
-	}
+    /**
+     * Constructor.
+     * Takes an already-configured {@see Pheanstalk} instance to work with.
+     * Does not attempt to establish a connection itself --
+     * use the {@see connect()} factory method for that instead.
+     *
+     * @param Pheanstalk $ph
+     */
+    public function __construct(Pheanstalk $ph)
+    {
+        $this->ph = $ph;
+    }
 
-	/**
-	 * Factory method.
-	 * This will create a new {@see Pheanstalk} instance by itself.
-	 *
-	 * See {@see Pheanstalk::__construct} for the parameter descriptions.
-	 *
-	 * @param string $host
-	 * @param int $port
-	 * @param int|null $connectTimeout
-	 * @return self
-	 */
-	public static function connect (string $host = "localhost", int $port = PheanstalkInterface::DEFAULT_PORT, int $connectTimeout = null) {
-		return new self (new Pheanstalk ($host, $port, $connectTimeout));
-	}
+    /**
+     * Factory method.
+     * This will create a new {@see Pheanstalk} instance by itself.
+     *
+     * See {@see Pheanstalk::__construct} for the parameter descriptions.
+     *
+     * @param string $host
+     * @param int $port
+     * @param int|null $connectTimeout
+     * @return self
+     */
+    public static function connect(
+        string $host = "localhost",
+        int $port = PheanstalkInterface::DEFAULT_PORT,
+        int $connectTimeout = null
+    ): self {
+        return new self (new Pheanstalk ($host, $port, $connectTimeout));
+    }
 
 
-	/**
-	 * This takes the next job from the named work queue
-	 * and returns it.
-	 *
-	 * @param string|string[] $workQueue The name(s) of the Work Queue(s) to poll.
-	 * @param int $timeout How many seconds to wait for a job to arrive, if none is available immediately.
-	 *                        Set this to NOBLOCK if the method should return immediately.
-	 *                        Set this to BLOCK if the call should block until a job becomes available, no matter how long it takes.
-	 * @return QueueEntry  Returns the next job in the work queue,
-	 *                     or NULL if no job was available after waiting for $timeout seconds.
-	 * @throws UnserializationException
-	 */
-	public function getNextQueueEntry ($workQueue, int $timeout = self::DEFAULT_TIMEOUT) : ?QueueEntry {
-		if ($timeout === WorkServerAdapter::FOREVER) {
-			// Beanstalkd has no real "forever" timeout option. This should be long enough...
-			$timeout = 60 * 60 * 24 * 365 * 10;
-		}
+    /**
+     * This takes the next job from the named work queue
+     * and returns it.
+     *
+     * @param string|string[] $workQueue The name(s) of the Work Queue(s) to poll.
+     * @param int $timeout               How many seconds to wait for a job to arrive, if none is available immediately.
+     *                                   Set this to NOBLOCK if the method should return immediately.
+     *                                   Set this to BLOCK if the call should block until a job becomes available, no matter how long it takes.
+     * @return QueueEntry  Returns the next job in the work queue,
+     *                                   or NULL if no job was available after waiting for $timeout seconds.
+     * @throws UnserializationException
+     */
+    public function getNextQueueEntry($workQueue, int $timeout = self::DEFAULT_TIMEOUT): ?QueueEntry
+    {
+        if ($timeout === WorkServerAdapter::FOREVER) {
+            // Beanstalkd has no real "forever" timeout option. This should be long enough...
+            $timeout = 60 * 60 * 24 * 365 * 10;
+        }
 
-		$this->enterWorkQueues((array)$workQueue);
+        $this->enterWorkQueues((array)$workQueue);
 
-		$jobHandle = $this->ph->reserve($timeout);
-		if (!$jobHandle) {
-			// timeout, no job available
-			return null;
-		}
+        $jobHandle = $this->ph->reserve($timeout);
+        if (!$jobHandle) {
+            // timeout, no job available
+            return null;
+        }
 
-		if (is_scalar($workQueue)) {
-			// Ok, we know where this job came from, as we were polling only one tube:
-			$fromWorkQueue = $workQueue;
-		} elseif (is_array($workQueue) && count($workQueue) === 1) {
-			// Ditto, just a different call syntax
-			$fromWorkQueue = reset($workQueue);
-		} else {
-			// We polled multiple tubes at once,
-			// so we'll have to ask the server about the job's origin:
-			$fromWorkQueue = ($this->ph->statsJob($jobHandle))['tube'];
-		}
+        if (is_scalar($workQueue)) {
+            // Ok, we know where this job came from, as we were polling only one tube:
+            $fromWorkQueue = $workQueue;
+        } elseif (is_array($workQueue) && count($workQueue) === 1) {
+            // Ditto, just a different call syntax
+            $fromWorkQueue = reset($workQueue);
+        } else {
+            // We polled multiple tubes at once,
+            // so we'll have to ask the server about the job's origin:
+            $fromWorkQueue = ($this->ph->statsJob($jobHandle))['tube'];
+        }
 
-		try {
-			return QueueEntry::fromSerializedJob(
-				$jobHandle->getData(),
-				$fromWorkQueue,
-				$jobHandle,
-				$jobHandle->getId() );
-		} catch (UnserializationException $e) {
-			$this->ph->bury($jobHandle);
-			throw $e;
-		}
-	}
+        try {
+            return QueueEntry::fromSerializedJob(
+                $jobHandle->getData(),
+                $fromWorkQueue,
+                $jobHandle,
+                $jobHandle->getId());
+        } catch (UnserializationException $e) {
+            $this->ph->bury($jobHandle);
+            throw $e;
+        }
+    }
 
-	/**
-	 * Make sure we're WATCHing the correct tubes
-	 * and nothing else.
-	 *
-	 * @param string[] $workQueues
-	 */
-	private function enterWorkQueues (array $workQueues) {
-		$watch_tubes  = array_diff($workQueues, $this->lastWatched);
-		$ignore_tubes = array_diff($this->lastWatched, $workQueues);
+    /**
+     * Make sure we're WATCHing the correct tubes
+     * and nothing else.
+     *
+     * @param string[] $workQueues
+     */
+    private function enterWorkQueues(array $workQueues)
+    {
+        $watch_tubes  = array_diff($workQueues, $this->lastWatched);
+        $ignore_tubes = array_diff($this->lastWatched, $workQueues);
 
-		foreach ($watch_tubes as $t) {
-			$this->ph->watch($t);
-		}
-		foreach ($ignore_tubes as $t) {
-			$this->ph->ignore($t);
-		}
+        foreach ($watch_tubes as $t) {
+            $this->ph->watch($t);
+        }
+        foreach ($ignore_tubes as $t) {
+            $this->ph->ignore($t);
+        }
 
-		$this->lastWatched = $workQueues;
-	}
+        $this->lastWatched = $workQueues;
+    }
 
-	/**
-	 * Stores a job in the work queue for later processing.
-	 *
-	 * @param string $workQueue The name of the Work Queue to store the job in.
-	 * @param Job $job The job to store.
-	 * @param int $delay  The job delay in seconds after which it will become available to {@see getNextQueueEntry()}.
-	 *                    Set to zero (default) for jobs which should be processed as soon as possible.
-	 */
-	public function storeJob (string $workQueue, Job $job, int $delay = 0) {
-		$serializedJob = serialize($job);
+    /**
+     * Stores a job in the work queue for later processing.
+     *
+     * @param string $workQueue The name of the Work Queue to store the job in.
+     * @param Job $job          The job to store.
+     * @param int $delay        The job delay in seconds after which it will become available to {@see getNextQueueEntry()}.
+     *                          Set to zero (default) for jobs which should be processed as soon as possible.
+     */
+    public function storeJob(string $workQueue, Job $job, int $delay = 0): void
+    {
+        $serializedJob = serialize($job);
 
-		$this->ph->putInTube($workQueue, $serializedJob, PheanstalkInterface::DEFAULT_PRIORITY, $delay);
-	}
+        $this->ph->putInTube($workQueue, $serializedJob, PheanstalkInterface::DEFAULT_PRIORITY, $delay);
+    }
 
-	/**
-	 * Buries an existing job
-	 * so that it won't be returned by {@see getNextQueueEntry()} again
-	 * but is still present in the system for manual inspection.
-	 *
-	 * This is what happens to failed jobs.
-	 *
-	 * @param QueueEntry $entry
-	 */
-	public function buryEntry (QueueEntry $entry) {
-		$this->ph->bury($entry->getHandle());
-	}
+    /**
+     * Buries an existing job
+     * so that it won't be returned by {@see getNextQueueEntry()} again
+     * but is still present in the system for manual inspection.
+     *
+     * This is what happens to failed jobs.
+     *
+     * @param QueueEntry $entry
+     */
+    public function buryEntry(QueueEntry $entry): void
+    {
+        $this->ph->bury($entry->getHandle());
+    }
 
-	/**
-	 * Re-queues an existing job
-	 * so that it can be returned by {@see getNextQueueEntry()}
-	 * again at some later time.
-	 * A {@see $delay} is required
-	 * to prevent the job from being returned right after it was re-queued.
-	 *
-	 * This is what happens to failed jobs which can still be re-queued for a retry.
-	 *
-	 * @param QueueEntry $entry The job to re-queue. The instance should not be used anymore after this call.
-	 * @param int $delay The job delay in seconds. It will become available for {@see getNextQueueEntry()} after this delay.
-	 * @param string|null $workQueue By default, to job is re-queued into its original Work Queue ({@see QueueEntry::getWorkQueue}).
-	 *                                With this parameter, a different Work Queue can be chosen.
-	 */
-	public function requeueEntry (QueueEntry $entry, int $delay, string $workQueue = null) {
-		// Sadly, we cannot use Beanstalk's RELEASE function here --
-		// the Job's serialization may have changed.
+    /**
+     * Re-queues an existing job
+     * so that it can be returned by {@see getNextQueueEntry()}
+     * again at some later time.
+     * A {@see $delay} is required
+     * to prevent the job from being returned right after it was re-queued.
+     *
+     * This is what happens to failed jobs which can still be re-queued for a retry.
+     *
+     * @param QueueEntry $entry       The job to re-queue. The instance should not be used anymore after this call.
+     * @param int $delay              The job delay in seconds. It will become available for {@see getNextQueueEntry()} after this delay.
+     * @param string|null $workQueue  By default, to job is re-queued into its original Work Queue ({@see QueueEntry::getWorkQueue}).
+     *                                With this parameter, a different Work Queue can be chosen.
+     */
+    public function requeueEntry(QueueEntry $entry, int $delay, string $workQueue = null): void
+    {
+        // Sadly, we cannot use Beanstalk's RELEASE function here --
+        // the Job's serialization may have changed.
 
-		$queue = $workQueue ?? $entry->getWorkQueue();
+        $queue = $workQueue ?? $entry->getWorkQueue();
 
-		$this->storeJob($queue, $entry->getJob(), $delay);
-		$this->ph->delete($entry->getHandle());
-	}
+        $this->storeJob($queue, $entry->getJob(), $delay);
+        $this->ph->delete($entry->getHandle());
+    }
 
-	/**
-	 * Permanently deletes a job entry for its work queue.
-	 *
-	 * This is what happens to finished jobs.
-	 *
-	 * @param QueueEntry $entry The job to delete.
-	 */
-	public function deleteEntry (QueueEntry $entry) {
-		$this->ph->delete($entry->getHandle());
-	}
+    /**
+     * Permanently deletes a job entry for its work queue.
+     *
+     * This is what happens to finished jobs.
+     *
+     * @param QueueEntry $entry The job to delete.
+     */
+    public function deleteEntry(QueueEntry $entry): void
+    {
+        $this->ph->delete($entry->getHandle());
+    }
 
 }
--- test/10-BeanstalkdServerTest.php
+++ test/10-BeanstalkdServerTest.php
@@ -9,78 +9,81 @@ use Pheanstalk\Exception\ConnectionException;
 use Pheanstalk\Pheanstalk;
 use Pheanstalk\PheanstalkInterface;
 
-require_once __DIR__.'/../vendor/mle86/wq/test/helper/AbstractWorkServerAdapterTest.php';
+require_once __DIR__ . '/../vendor/mle86/wq/test/helper/AbstractWorkServerAdapterTest.php';
 
 class BeanstalkdServerTest
-	extends AbstractWorkServerAdapterTest
+    extends AbstractWorkServerAdapterTest
 {
 
-	public function checkEnvironment () {
-		$this->checkInDocker();
+    public function checkEnvironment(): void
+    {
+        $this->checkInDocker();
 
-		$this->assertTrue((getenv('BEANSTALKD_PORT') !== false),
-			"No BEANSTALKD_PORT ENV variable found! Is this test running in the test container?");
-		$this->assertGreaterThan(1024, getenv('BEANSTALKD_PORT'),
-			"Invalid BEANSTALKD_PORT ENV variable!");
-		$this->assertNotEquals(PheanstalkInterface::DEFAULT_PORT , getenv('BEANSTALKD_PORT'),
-			"BEANSTALKD_PORT ENV variable should NOT be set to the default Beanstalkd port! " .
-			"This prevents the test scripts from accidentally running on the host system.");
+        $this->assertNotFalse(getenv('BEANSTALKD_PORT'),
+            "No BEANSTALKD_PORT ENV variable found! Is this test running in the test container?");
+        $this->assertGreaterThan(1024, getenv('BEANSTALKD_PORT'),
+            "Invalid BEANSTALKD_PORT ENV variable!");
+        $this->assertNotEquals(PheanstalkInterface::DEFAULT_PORT, getenv('BEANSTALKD_PORT'),
+            "BEANSTALKD_PORT ENV variable should NOT be set to the default Beanstalkd port! " .
+            "This prevents the test scripts from accidentally running on the host system.");
 
-		$e = null;
-		try {
-			(new BeanstalkdWorkServer (new Pheanstalk ("localhost", PheanstalkInterface::DEFAULT_PORT)))
-				->getNextQueueEntry("@this-should-not-exist-29743984375345", BeanstalkdWorkServer::NOBLOCK);
-		} catch (ConnectionException $e) {
-			// ok
-		}
-		$this->assertInstanceOf(\Exception::class, $e,
-			"We managed to get a Beanstalkd connection on the Beanstalk default port! " .
-			"This should not be possible inside the test container.");
-	}
+        $e = null;
+        try {
+            (new BeanstalkdWorkServer (new Pheanstalk ("localhost", PheanstalkInterface::DEFAULT_PORT)))
+                ->getNextQueueEntry("@this-should-not-exist-29743984375345", BeanstalkdWorkServer::NOBLOCK);
+        } catch (ConnectionException $e) {
+            // ok
+        }
+        $this->assertInstanceOf(\Exception::class, $e,
+            "We managed to get a Beanstalkd connection on the Beanstalk default port! " .
+            "This should not be possible inside the test container.");
+    }
 
-	public function getWorkServerAdapter () : WorkServerAdapter {
-		return BeanstalkdWorkServer::connect("localhost", (int)getenv('BEANSTALKD_PORT'));
-	}
+    public function getWorkServerAdapter(): WorkServerAdapter
+    {
+        return BeanstalkdWorkServer::connect("localhost", (int)getenv('BEANSTALKD_PORT'));
+    }
 
-	public function additionalTests (WorkServerAdapter $ws) {
-		$this->checkDefaultTube($ws);
-	}
+    public function additionalTests(WorkServerAdapter $ws): void
+    {
+        $this->checkDefaultTube($ws);
+    }
 
-	/**
-	 * By default, all Beanstalkd clients are WATCHing the implicit "default" tube.
-	 * Make sure we're NOT doing that.
-	 *
-	 * @param WorkServerAdapter $ws
-	 */
-	protected function checkDefaultTube (WorkServerAdapter $ws) {
-		$queue_name = "default";
+    /**
+     * By default, all Beanstalkd clients are WATCHing the implicit "default" tube.
+     * Make sure we're NOT doing that.
+     *
+     * @param WorkServerAdapter $ws
+     */
+    protected function checkDefaultTube(WorkServerAdapter $ws)
+    {
+        $queue_name = "default";
 
-		$j = new SimpleJob (7660);
-		$ws->storeJob($queue_name, $j);
+        $j = new SimpleJob (7660);
+        $ws->storeJob($queue_name, $j);
 
-		// new connection!
-		$ws = $this->getWorkServerAdapter();
+        // new connection!
+        $ws = $this->getWorkServerAdapter();
 
-		// This is a new connection. It has never heard of any Job instance nor any queue names.
-		// It should be unable to retrieve that job by polling an unrelated queue name.
-		$ret = $ws->getNextQueueEntry(["unrelated-empty-queue-403165009"], $ws::NOBLOCK);
-		$this->assertNull($ret,
-			"We're still subscribed to Beanstalkd's implicit '{$queue_name}' tube! \n" .
-			"(We successfully retrieved a job in the '{$queue_name}' tube by polling a completely different tube)");
+        // This is a new connection. It has never heard of any Job instance nor any queue names.
+        // It should be unable to retrieve that job by polling an unrelated queue name.
+        $ret = $ws->getNextQueueEntry(["unrelated-empty-queue-403165009"], $ws::NOBLOCK);
+        $this->assertNull($ret,
+            "We're still subscribed to Beanstalkd's implicit '{$queue_name}' tube! \n" .
+            "(We successfully retrieved a job in the '{$queue_name}' tube by polling a completely different tube)");
 
-		// ...but if we really want, we can definitely poll it:
-		$ret = $ws->getNextQueueEntry(["unrelated-empty-queue-403165009", $queue_name], $ws::NOBLOCK);
-		$this->assertInstanceOf(QueueEntry::class, $ret,
-			"We were unable to poll a job from Beanstalkd's implicit '{$queue_name}' tube " .
-			"after first ignoring and then re-watching it!");
+        // ...but if we really want, we can definitely poll it:
+        $ret = $ws->getNextQueueEntry(["unrelated-empty-queue-403165009", $queue_name], $ws::NOBLOCK);
+        $this->assertInstanceOf(QueueEntry::class, $ret,
+            "We were unable to poll a job from Beanstalkd's implicit '{$queue_name}' tube " .
+            "after first ignoring and then re-watching it!");
 
-		/** @var Job|SimpleJob $qj */
-		$qj = $ret->getJob();
-		$this->assertSame($j->getMarker(), $qj->getMarker(),
-			"We got an UNEXPECTED job from the '{$queue_name}' tube!");
-		$this->assertSame($queue_name, $ret->getWorkQueue(),
-			"The job retrieved from the '{$queue_name}' tube contains an incorrect origin reference!");
-	}
+        /** @var Job|SimpleJob $qj */
+        $qj = $ret->getJob();
+        $this->assertSame($j->getMarker(), $qj->getMarker(),
+            "We got an UNEXPECTED job from the '{$queue_name}' tube!");
+        $this->assertSame($queue_name, $ret->getWorkQueue(),
+            "The job retrieved from the '{$queue_name}' tube contains an incorrect origin reference!");
+    }
 
 }
-
