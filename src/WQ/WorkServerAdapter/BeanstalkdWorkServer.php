65f2bd899e035562af03f0b97b1c243d6978ec8b
mle <maximilian@eul.cc>
2018-07-09 22:53:19 +0200

    doc: cleanup

--- src/WQ/WorkServerAdapter/BeanstalkdWorkServer.php
+++ src/WQ/WorkServerAdapter/BeanstalkdWorkServer.php
@@ -64,18 +64,6 @@ class BeanstalkdWorkServer
     }
 
 
-    /**
-     * This takes the next job from the named work queue
-     * and returns it.
-     *
-     * @param string|string[] $workQueue The name(s) of the Work Queue(s) to poll.
-     * @param int $timeout               How many seconds to wait for a job to arrive, if none is available immediately.
-     *                                   Set this to NOBLOCK if the method should return immediately.
-     *                                   Set this to BLOCK if the call should block until a job becomes available, no matter how long it takes.
-     * @return QueueEntry  Returns the next job in the work queue,
-     *                                   or NULL if no job was available after waiting for $timeout seconds.
-     * @throws UnserializationException
-     */
     public function getNextQueueEntry($workQueue, int $timeout = self::DEFAULT_TIMEOUT): ?QueueEntry
     {
         if ($timeout === WorkServerAdapter::FOREVER) {
@@ -136,14 +124,6 @@ class BeanstalkdWorkServer
         $this->lastWatched = $workQueues;
     }
 
-    /**
-     * Stores a job in the work queue for later processing.
-     *
-     * @param string $workQueue The name of the Work Queue to store the job in.
-     * @param Job $job          The job to store.
-     * @param int $delay        The job delay in seconds after which it will become available to {@see getNextQueueEntry()}.
-     *                          Set to zero (default) for jobs which should be processed as soon as possible.
-     */
     public function storeJob(string $workQueue, Job $job, int $delay = 0): void
     {
         $serializedJob = serialize($job);
@@ -151,34 +131,11 @@ class BeanstalkdWorkServer
         $this->ph->putInTube($workQueue, $serializedJob, PheanstalkInterface::DEFAULT_PRIORITY, $delay);
     }
 
-    /**
-     * Buries an existing job
-     * so that it won't be returned by {@see getNextQueueEntry()} again
-     * but is still present in the system for manual inspection.
-     *
-     * This is what happens to failed jobs.
-     *
-     * @param QueueEntry $entry
-     */
     public function buryEntry(QueueEntry $entry): void
     {
         $this->ph->bury($entry->getHandle());
     }
 
-    /**
-     * Re-queues an existing job
-     * so that it can be returned by {@see getNextQueueEntry()}
-     * again at some later time.
-     * A {@see $delay} is required
-     * to prevent the job from being returned right after it was re-queued.
-     *
-     * This is what happens to failed jobs which can still be re-queued for a retry.
-     *
-     * @param QueueEntry $entry       The job to re-queue. The instance should not be used anymore after this call.
-     * @param int $delay              The job delay in seconds. It will become available for {@see getNextQueueEntry()} after this delay.
-     * @param string|null $workQueue  By default, to job is re-queued into its original Work Queue ({@see QueueEntry::getWorkQueue}).
-     *                                With this parameter, a different Work Queue can be chosen.
-     */
     public function requeueEntry(QueueEntry $entry, int $delay, string $workQueue = null): void
     {
         // Sadly, we cannot use Beanstalk's RELEASE function here --
@@ -190,13 +147,6 @@ class BeanstalkdWorkServer
         $this->ph->delete($entry->getHandle());
     }
 
-    /**
-     * Permanently deletes a job entry for its work queue.
-     *
-     * This is what happens to finished jobs.
-     *
-     * @param QueueEntry $entry The job to delete.
-     */
     public function deleteEntry(QueueEntry $entry): void
     {
         $this->ph->delete($entry->getHandle());
